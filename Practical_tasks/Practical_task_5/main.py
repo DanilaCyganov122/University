dd18c13f20cd00171c0f5a57d8ce32e377aa5405
DanilaCyganov122 <144428593+DanilaCyganov122@users.noreply.github.com>
2023-11-10 16:04:42 +0300

    practicaltask345

--- Practical_tasks/Practical_task_5/main.py
+++ Practical_tasks/Practical_task_5/main.py
@@ -1,17 +1,21 @@
-while True:
-    try:
-        file_name = input("Введите имя файла: ")
-        file = open(file_name, mode='r', encoding='utf8')
-        break
-    except:
-        print("Некорректный ввод! Введите снова")
-number_list = file.readlines()
-file.close()
-number_list.pop(0)
-for i in range(0, len(number_list)):
-    number_list[i] = int((str(number_list[i])).replace('\n', ''))
 
-print(number_list)
+def error():
+    while True:
+        try:
+            file_name = input("Введите имя файла: ")
+            file = open(file_name, mode='r', encoding='utf8')
+            break
+        except:
+            print("Некорректный ввод! Введите снова")
+    number_list = file.readlines()
+    file.close()
+    number_list.pop(0)
+    for i in range(0, len(number_list)):
+        number_list[i] = int((str(number_list[i])).replace('\n', ''))
+    return number_list
+
+if __name__ == '__main__':
+    print(error())
 
 
 
